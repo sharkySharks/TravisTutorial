866b8970472dd46f84fe6895f9ec5fbe1a2bb7d2
Sharky <kaydenalthen@gmail.com>
2015-05-02 10:52:14 -0500

    first build

--- hello.js
+++ hello.js
@@ -5,4 +5,4 @@ http.createServer(function (req, res) {
   res.end('Hello World\n'); // missing semi-colon will fail the build
 }).listen(1337, '127.0.0.1');
  
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:1337/')
